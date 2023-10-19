01ca12612b2224637ce6169c2edc947781bf658b
marcelogomesandrade <gomesgomes123321@gmail.com>
2023-10-19 16:32:23 -0300

    9

---
+++ js/menu.js
@@ -0,0 +1,16 @@
+"use strict";
+
+//Selecionando o elemento que acionará o menu / através de descendência
+
+const botaoMenu = document.querySelector ("nav h2 ");
+
+// Selecionando a lista/Menu 
+
+const menu = document.querySelector (".menu");
+
+//Selecionando o link que está dentro do nav h2
+
+const textBotao = botaoMenu.querySelector ("a");
+
+
+
